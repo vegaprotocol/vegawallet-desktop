47d465efa304c7f5655bec4ec9e3039255c883da
Matthew Russell <mattrussell36@gmail.com>
2021-08-20 23:17:27 -0700

    add route for wallet creation

--- frontend/src/routes/import/import-select.tsx
+++ frontend/src/routes/import/import-select.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Link, useRouteMatch } from 'react-router-dom'
-import { BulletHeader } from '../../components/bullet-header'
-import { ButtonGroup } from '../../components/button-group'
+import React from "react";
+import { Link, useRouteMatch } from "react-router-dom";
+import { BulletHeader } from "../../components/bullet-header";
+import { ButtonGroup } from "../../components/button-group";
 
 export function ImportSelect() {
-  const match = useRouteMatch()
+  const match = useRouteMatch();
   return (
     <>
-      <BulletHeader tag='h1'>Import wallet</BulletHeader>
+      <BulletHeader tag="h1">Create or import wallet</BulletHeader>
+
       <ButtonGroup>
+        <Link to={`${match.path}/create`}>
+          <button className="fill">Create new</button>
+        </Link>
         <Link to={`${match.path}/path`}>
-          <button className='fill'>Import by path</button>
+          <button className="fill">Import by path</button>
         </Link>
         <Link to={`${match.path}/mnemonic`}>
-          <button className='fill'>Import by mnemonic</button>
+          <button className="fill">Import by mnemonic</button>
         </Link>
       </ButtonGroup>
     </>
-  )
+  );
 }
