8c95379631d4b7f0dc2c561dab1bcca78033040f
Valentin Trinque <val.trinque@gmail.com>
2021-08-24 11:45:37 +0200

    Add backend to list keys

---
+++ frontend/src/models/list-keys.ts
@@ -0,0 +1,23 @@
+export class ListKeysRequest {
+  Name: string = ''
+  Passphrase: string = ''
+}
+
+export class ListKeysResponse {
+  Name: string = ''
+  KeyPairs: KeyPair[] = []
+}
+
+export class KeyPair {
+  PublicKey: string = ''
+  PrivateKey: string = ''
+  IsTainted: boolean = false
+  Meta: Meta[] = []
+  AlgorithmVersion: number = 0
+  AlgorithmName: string = ''
+}
+
+export class Meta {
+  Key: string = ''
+  Value: string = ''
+}
