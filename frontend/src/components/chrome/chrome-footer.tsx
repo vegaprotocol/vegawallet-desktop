826753b3f003822388140a6d368689499190c029
Matthew Russell <mattrussell36@gmail.com>
2021-12-07 19:05:42 -0800

    add status circle for footer

--- frontend/src/components/chrome/chrome-footer.tsx
+++ frontend/src/components/chrome/chrome-footer.tsx
@@ -22,25 +22,46 @@ export function ChromeFooter() {
         backgroundSize: 'cover'
       }}>
       <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
-        {serviceRunning ? (
-          <div>Service running: {serviceUrl}</div>
-        ) : (
-          <div>Service not running</div>
-        )}
-        {consoleRunning ? (
-          <div>
-            Console running:{' '}
-            <ExternalLink
-              href={consoleUrl}
-              style={{ textDecoration: 'underline' }}>
-              {consoleUrl}
-            </ExternalLink>
-          </div>
-        ) : (
-          <div>Console not running</div>
-        )}
+        <div>
+          <StatusCircle running={serviceRunning} />
+          {serviceRunning ? (
+            <>Service running: {serviceUrl}</>
+          ) : (
+            <>Service not running</>
+          )}
+        </div>
+        <div>
+          <StatusCircle running={consoleRunning} />
+          {consoleRunning ? (
+            <>
+              Console running:{' '}
+              <ExternalLink
+                href={consoleUrl}
+                style={{ textDecoration: 'underline' }}>
+                {consoleUrl}
+              </ExternalLink>
+            </>
+          ) : (
+            <>Console not running</>
+          )}
+        </div>
       </div>
       <div>Version {version}</div>
     </footer>
   )
+}
+
+export function StatusCircle({ running }: any) {
+  const baseStyles: React.CSSProperties = {
+    display: 'inline-block',
+    width: 11,
+    height: 11,
+    borderRadius: '50%',
+    border: '2px solid white',
+    marginRight: 5
+  }
+  const contextualStyles: React.CSSProperties = {
+    background: running ? 'white' : 'transparent'
+  }
+  return <span style={{ ...baseStyles, ...contextualStyles }} />
 }
