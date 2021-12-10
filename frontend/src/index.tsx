6fc3efb63eddbf358b86b6f6005a1b8b096306c2
Matthew Russell <mattrussell36@gmail.com>
2021-12-09 16:04:31 -0800

    update todos

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,15 +8,10 @@ import * as Wails from '@wailsapp/runtime'
 
 /**
 TODO:
-- Permission for import wallet by path
 - Unmarshal request json for import by mnemonic
 - chevron for network switcher
-- Update nodes list
-- Separate start console proxy from start service
-- Stop service when network changes (Can we avoid stopping and starting service if the network changes?)
 - Investigate input type file for import path
 - After create go straight to keys (store password only once for memory)
-- Test no network configs
 - Importing configs? Where to prompt?
 - Tests
 */
