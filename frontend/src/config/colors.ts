0954c5116625ff14ba54522d827bb7a1e838ea99
Matthew Russell <mattrussell36@gmail.com>
2022-01-17 12:29:46 -0800

    add styles for form group

--- frontend/src/config/colors.ts
+++ frontend/src/config/colors.ts
@@ -1,10 +1,22 @@
+import { Intent } from './intent'
+
 export const Colors = {
   WHITE: '#FFF',
   RED: '#ED1515',
   PINK: '#ff077f',
   GREEN: '#26ff8a',
+  ORANGE: '#d9822b',
   DARK_GRAY_1: '#1f1f1f',
   DARK_GRAY_2: '#2a2a2a',
   DARK_GRAY_3: '#363636',
-  DARK_GRAY_5: '#494949'
+  DARK_GRAY_5: '#494949',
+  TEXT_MUTED: '#8a9ba8'
+}
+
+export const IntentColors: { [i in Intent]: string } = {
+  none: 'inherit',
+  primary: 'inherit',
+  success: Colors.GREEN,
+  warning: Colors.ORANGE,
+  danger: Colors.RED
 }
