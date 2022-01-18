91e348554564ae2182f17b87266c0c9490edb962
Matthew Russell <mattrussell36@gmail.com>
2022-01-17 19:15:11 -0800

    remove unused files

--- frontend/src/components/success-message.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const SuccessMessage = (props: any): JSX.Element | null => {
-  if (props.message === null) {
-    return null
-  }
-  return <p style={{ color: 'green' }}>{props.message}</p>
-}
