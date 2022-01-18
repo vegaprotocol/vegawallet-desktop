8993c29b9aa7f9bd4f10fc020d5aff81e5fb3c8b
Matthew Russell <mattrussell36@gmail.com>
2022-01-17 23:14:35 -0800

    fix import buttons spacing

--- frontend/src/routes/import/import-select.tsx
+++ frontend/src/routes/import/import-select.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import { ImportPaths } from '.'
 import { BulletHeader } from '../../components/bullet-header'
 import { Button } from '../../components/button'
 import { ButtonGroup } from '../../components/button-group'
 
 export function ImportSelect() {
-  const location = useLocation()
+  const history = useHistory()
   return (
     <>
       <BulletHeader tag='h1'>Create or import wallet</BulletHeader>
@@ -18,12 +18,10 @@ export function ImportSelect() {
             text: 'Import by recovery phrase'
           }
         ].map(route => {
-          const isActive = location.pathname === route.path
-          const className = ['fill', isActive ? 'active' : ''].join(' ')
           return (
-            <Link to={route.path} key={route.path}>
-              <Button className={className}>{route.text}</Button>
-            </Link>
+            <Button key={route.path} onClick={() => history.push(route.path)}>
+              {route.text}
+            </Button>
           )
         })}
       </ButtonGroup>
