ea81f48edb6a302ee36b77b776f81ddb52e0dfcc
Matthew Russell <mattrussell36@gmail.com>
2022-01-17 19:08:20 -0800

    fix tooltips

--- frontend/src/components/tooltip/tooltip.tsx
+++ frontend/src/components/tooltip/tooltip.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import { Colors } from '../../config/colors'
 
 interface TooltipProps {
   trigger: React.ReactElement
@@ -11,8 +12,12 @@ export function Tooltip({ trigger, content, isOpen }: TooltipProps) {
   return (
     <TooltipPrimitive.Provider>
       <TooltipPrimitive.Root open={isOpen}>
-        {trigger}
-        <TooltipPrimitive.Content>{content}</TooltipPrimitive.Content>
+        <TooltipPrimitive.Trigger>{trigger}</TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Content
+          style={{ background: Colors.DARK_GRAY_2, padding: '5px 10px' }}>
+          <TooltipPrimitive.Arrow fill={Colors.DARK_GRAY_2} />
+          {content}
+        </TooltipPrimitive.Content>
       </TooltipPrimitive.Root>
     </TooltipPrimitive.Provider>
   )
