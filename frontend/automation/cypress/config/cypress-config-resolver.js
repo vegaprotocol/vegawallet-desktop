fb7953990e778425443221d818e16bba3858fb95
Dexter <dexter.edwards93@gmail.com>
2022-01-26 10:25:31 +0000

    make coverage work again=

--- frontend/automation/cypress/config/cypress-config-resolver.js
+++ frontend/automation/cypress/config/cypress-config-resolver.js
@@ -1,19 +1,16 @@
-const path = require("path");
-const deepmerge = require("deepmerge");
+const path = require('path')
+const deepmerge = require('deepmerge')
 
-const cypressConfigResolver = (config) => {
-  let configJson = require(config.configFile);
+const cypressConfigResolver = config => {
+  let configJson = require(config.configFile)
   if (configJson.extends) {
-    const baseConfigFilename = path.join(
-      config.projectRoot,
-      configJson.extends
-    );
-    const baseConfig = require(baseConfigFilename);
-    console.log("merging %s with %s", baseConfigFilename, config.configFile);
-    configJson = deepmerge(baseConfig, configJson);
+    const baseConfigFilename = path.join(config.projectRoot, configJson.extends)
+    const baseConfig = require(baseConfigFilename)
+    console.log('merging %s with %s', baseConfigFilename, config.configFile)
+    configJson = deepmerge(baseConfig, configJson)
   }
 
-  return configJson;
-};
+  return deepmerge(config, configJson)
+}
 
-module.exports.cypressConfigResolver = cypressConfigResolver;
+module.exports.cypressConfigResolver = cypressConfigResolver
--- frontend/automation/cypress/plugins/index.js
+++ frontend/automation/cypress/plugins/index.js
@@ -7,20 +7,20 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-const { cypressConfigResolver } = require("../config/cypress-config-resolver");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+const { cypressConfigResolver } = require('../config/cypress-config-resolver')
+const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
-  require("@cypress/code-coverage/task")(on, config);
-  on("file:preprocessor", cucumber());
+  require('@cypress/code-coverage/task')(on, config)
+  on('file:preprocessor', cucumber())
   // Prevent test crashes
   // https://github.com/cypress-io/cypress/issues/7204#issuecomment-810774449
   // https://developers.google.com/web/tools/puppeteer/troubleshooting#tips
-  on("before:browser:launch", (browser, launchOptions) => {
-    if (browser.family === "chromium") {
-      launchOptions.args.push("--disable-dev-shm-usage");
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.family === 'chromium') {
+      launchOptions.args.push('--disable-dev-shm-usage')
     }
-    return launchOptions;
-  });
-  return cypressConfigResolver(config);
-};
+    return launchOptions
+  })
+  return cypressConfigResolver(config)
+}
