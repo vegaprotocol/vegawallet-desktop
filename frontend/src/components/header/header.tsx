43126487b8e933e47454b026671100b4b5ba1b40
Matthew Russell <mattrussell36@gmail.com>
2022-01-31 12:20:03 -0800

    use header component more for network info table

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 interface Header {
   children: React.ReactNode
+  style?: React.CSSProperties
 }
 
-export const Header = ({ children }: Header) => {
+export const Header = ({ children, style }: Header) => {
   return (
     <h1
       style={{
         fontSize: 22,
-        margin: '2px 0 0 0',
-        padding: '0 0 25px',
+        margin: '30px 0 10px 0',
         textTransform: 'uppercase',
         letterSpacing: '0.3em',
-        lineHeight: 1.2
-      }}
-    >
+        lineHeight: 1.2,
+        ...style
+      }}>
       {children}
     </h1>
   )
