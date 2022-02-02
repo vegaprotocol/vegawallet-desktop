7f121956ab82cabff1ebf726ef1b959f8c5fad3d
Matthew Russell <mattrussell36@gmail.com>
2022-02-01 19:12:08 -0800

    navigate to wallet page after import wallet

--- frontend/src/routes/wallet-import/import-recovery-phrase.tsx
+++ frontend/src/routes/wallet-import/import-recovery-phrase.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Header } from '../../components/header'
 import { WalletImportForm } from '../../components/wallet-import-form'
 import { useImportWallet } from '../../hooks/use-import-wallet'
+import { Paths } from '../router-config'
 
 export const ImportRecoveryPhrase = () => {
-  const { submit } = useImportWallet()
+  const history = useHistory()
+  const { submit, response } = useImportWallet()
+
+  React.useEffect(() => {
+    if (response) {
+      history.push(Paths.Wallet)
+    }
+  }, [response, history])
+
   return (
     <>
       <Header style={{ marginTop: 0 }}>Import wallet</Header>
