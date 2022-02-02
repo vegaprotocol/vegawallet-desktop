1c39029de3885426a2e03de655aadb523a183e21
Matthew Russell <mattrussell36@gmail.com>
2022-02-01 21:30:43 -0800

    start dapps from console drawer

--- frontend/src/components/chrome/chrome.tsx
+++ frontend/src/components/chrome/chrome.tsx
@@ -1,26 +1,18 @@
 import React from 'react'
 import { Colors } from '../../config/colors'
-import { ChromeSidebar } from './chrome-sidebar'
 import { ChromeDrawer } from './chrome-drawer'
 
 export const DRAWER_HEIGHT = 70
-
-const layoutStyles: React.CSSProperties = {
-  display: 'grid',
-  gridTemplateColumns: 'min-content 1fr',
-  gridTemplateRows: '1fr',
-  paddingBottom: DRAWER_HEIGHT
-}
 
 /**
  * Handles app layout for main content, sidebar and footer
  */
 export function Chrome({ children }: { children: React.ReactNode }) {
   return (
-    <div style={{ height: '100%' }}>
+    <>
       <div
         style={{
-          ...layoutStyles,
+          paddingBottom: DRAWER_HEIGHT,
           height: '100%',
           background: Colors.BLACK,
           overflowY: 'auto'
@@ -29,19 +21,8 @@ export function Chrome({ children }: { children: React.ReactNode }) {
         <main style={{ gridColumn: '2 / 3', padding: 20, overflowY: 'auto' }}>
           {children}
         </main>
-        <div
-          style={{
-            backgroundRepeat: 'no-repeat',
-            gridColumn: '1 / 2',
-            gridRow: '1 / 3',
-            minWidth: 240,
-            background: Colors.DARK_GRAY_1
-          }}
-        >
-          <ChromeSidebar />
-        </div>
       </div>
       <ChromeDrawer />
-    </div>
+    </>
   )
 }
--- frontend/src/components/network-info/network-info.tsx
+++ frontend/src/components/network-info/network-info.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Intent } from '../../config/intent'
 import { useNetwork } from '../../contexts/network/network-context'
-import { ExternalLink } from '../external-link'
+import { startProxyAction } from '../../contexts/service/service-actions'
+import { ProxyApp, useService } from '../../contexts/service/service-context'
+import { Network } from '../../models/network'
+import { ButtonUnstyled } from '../button-unstyled'
 import { Header } from '../header'
 import { NodeList } from '../node-list'
+import { AppToaster } from '../toaster'
 
 export function NetworkInfo() {
   const {
@@ -39,23 +44,8 @@ export function NetworkInfo() {
           </tr>
         </tbody>
       </table>
-      <Header>Console</Header>
-      <table>
-        <tbody>
-          <tr>
-            <th>URL</th>
-            <td>
-              <ExternalLink href={`https://${config.Console.URL}`}>
-                {config.Console.URL}
-              </ExternalLink>
-            </td>
-          </tr>
-          <tr>
-            <th>Local port</th>
-            <td>{config.Console.LocalPort}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Header>Apps</Header>
+      <DAppsTable config={config} />
       <Header>gRPC Nodes</Header>
       <NodeList items={config.API.GRPC.Hosts} />
       <Header>GraphQL Nodes</Header>
@@ -63,5 +53,63 @@ export function NetworkInfo() {
       <Header>REST Nodes</Header>
       <NodeList items={config.API.REST.Hosts} />
     </>
+  )
+}
+
+interface DAppsTableProps {
+  config: Network
+}
+
+function DAppsTable({ config }: DAppsTableProps) {
+  const {
+    state: { network }
+  } = useNetwork()
+  const { dispatch } = useService()
+
+  function startProxy(app: ProxyApp) {
+    if (!network || !config || app === ProxyApp.None) {
+      AppToaster.show({
+        message: 'No network config found',
+        intent: Intent.DANGER
+      })
+      return
+    }
+
+    const port =
+      app === ProxyApp.Console
+        ? config.Console.LocalPort
+        : config.TokenDApp.LocalPort
+
+    dispatch(startProxyAction(network, app, port))
+  }
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Console</th>
+          <td>
+            <ButtonUnstyled
+              style={{ textAlign: 'right' }}
+              onClick={() => startProxy(ProxyApp.Console)}
+            >
+              Start {config.Console.URL} on http://127.0.0.1:
+              {config.Console.LocalPort}
+            </ButtonUnstyled>
+          </td>
+        </tr>
+        <tr>
+          <th>Token App</th>
+          <td>
+            <ButtonUnstyled
+              style={{ textAlign: 'right' }}
+              onClick={() => startProxy(ProxyApp.TokenDApp)}
+            >
+              Start {config.TokenDApp.URL} on http://127.0.0.1:
+              {config.TokenDApp.LocalPort}
+            </ButtonUnstyled>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
