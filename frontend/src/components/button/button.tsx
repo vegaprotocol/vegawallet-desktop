fa11db4480bdf9393b463284c1b24b6c89be5287
Matthew Russell <mattrussell36@gmail.com>
2022-02-03 11:07:52 -0800

    add hover state to buttons

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -2,26 +2,17 @@ import React, { ButtonHTMLAttributes, ForwardedRef } from 'react'
 import { Colors } from '../../config/colors'
 import { Spinner } from '../spinner'
 
-const style: React.CSSProperties = {
-  background: 'transparent',
-  border: `1px solid ${Colors.WHITE}`,
-  borderRadius: 0,
-  color: Colors.WHITE,
-  cursor: 'pointer',
-  fontSize: 16,
-  padding: '7px 17px',
-  textTransform: 'uppercase',
-  minWidth: 145
-}
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   loading?: boolean
 }
 
 export const Button = React.forwardRef(
   (
-    { children, loading, ...props }: ButtonProps,
+    { children, loading, onMouseEnter, onMouseLeave, ...props }: ButtonProps,
     ref: ForwardedRef<HTMLButtonElement>
   ) => {
+    const [hover, setHover] = React.useState(false)
+
     const renderChildren = () => {
       if (loading) {
         return <Spinner />
@@ -30,11 +21,44 @@ export const Button = React.forwardRef(
       return children
     }
 
+    const handleMouseEnter = (
+      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      setHover(true)
+      if (typeof onMouseEnter === 'function') {
+        onMouseEnter(e)
+      }
+    }
+
+    const handleMouseLeave = (
+      e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      setHover(false)
+      if (typeof onMouseEnter === 'function') {
+        onMouseEnter(e)
+      }
+    }
+
+    const style: React.CSSProperties = {
+      background: hover ? Colors.WHITE : 'transparent',
+      color: hover ? Colors.BLACK : Colors.WHITE,
+      border: `1px solid ${Colors.WHITE}`,
+      borderRadius: 0,
+      cursor: 'pointer',
+      fontSize: 16,
+      padding: '7px 17px',
+      textTransform: 'uppercase',
+      minWidth: 145,
+      transition: 'all .3s ease'
+    }
+
     return (
       <button
         ref={ref}
         type='button'
         {...props}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         disabled={loading ? true : props.disabled}
         style={{ ...style, ...props.style }}
       >
