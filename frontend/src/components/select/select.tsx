e4814221e551d59776b972e2c9d31c5f775e8559
Matthew Russell <mattrussell36@gmail.com>
2022-02-22 11:23:48 -0800

    wrap select with forward ref so react-hook-form binds properly

--- frontend/src/components/select/select.tsx
+++ frontend/src/components/select/select.tsx
@@ -1,4 +1,5 @@
-import type { SelectHTMLAttributes } from 'react'
+import type { ForwardedRef, SelectHTMLAttributes } from 'react'
+import { forwardRef } from 'react'
 
 import { Colors } from '../../config/colors'
 
@@ -6,23 +7,25 @@ type SelectProps = SelectHTMLAttributes<HTMLSelectElement>
 
 const CARET_SIZE = 5
 
-export function Select({ style, ...props }: SelectProps) {
-  return (
-    <div style={{ position: 'relative' }}>
-      <select {...props} style={{ ...style, paddingRight: 20 }} />
-      <span
-        style={{
-          position: 'absolute',
-          display: 'block',
-          top: `calc(50% - ${CARET_SIZE / 2}px)`,
-          right: 10,
-          width: 0,
-          height: 0,
-          borderTop: `${CARET_SIZE}px solid ${Colors.WHITE}`,
-          borderRight: `${CARET_SIZE}px solid transparent`,
-          borderLeft: `${CARET_SIZE}px solid transparent`
-        }}
-      />
-    </div>
-  )
-}
+export const Select = forwardRef(
+  ({ style, ...props }: SelectProps, ref: ForwardedRef<HTMLSelectElement>) => {
+    return (
+      <div style={{ position: 'relative' }}>
+        <select {...props} ref={ref} style={{ ...style, paddingRight: 20 }} />
+        <span
+          style={{
+            position: 'absolute',
+            display: 'block',
+            top: `calc(50% - ${CARET_SIZE / 2}px)`,
+            right: 10,
+            width: 0,
+            height: 0,
+            borderTop: `${CARET_SIZE}px solid ${Colors.WHITE}`,
+            borderRight: `${CARET_SIZE}px solid transparent`,
+            borderLeft: `${CARET_SIZE}px solid transparent`
+          }}
+        />
+      </div>
+    )
+  }
+)
