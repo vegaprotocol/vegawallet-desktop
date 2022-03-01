d340870f227ca5b20e03b79a126edf1d04f00cf8
Dexter <dexter.edwards93@gmail.com>
2022-03-01 13:25:04 +0000

    use for CI based on env var

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import istanbul from 'vite-plugin-istanbul'
 
+const truthy = ['1', 'true']
+
 export default defineConfig(args => {
   return {
     build: {
@@ -16,8 +18,7 @@ export default defineConfig(args => {
         include: 'src/*',
         exclude: ['node_modules', 'automation/', 'src/wailsjs'],
         extension: ['.js', '.ts', '.tsx'],
-        // requireEnv: true,
-        forceBuildInstrument: true
+        forceBuildInstrument: truthy.includes(process.env['VITE_COVERAGE'])
       })
     ]
   }
