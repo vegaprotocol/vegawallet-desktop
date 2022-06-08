a38e5ef78db548659c0c47f911c44f0bf9cf1d99
Matthew Russell <mattrussell36@gmail.com>
2022-06-08 12:31:56 -0700

    chore: convert key val table to dl element

--- frontend/src/components/key-value-table/key-value-table.tsx
+++ frontend/src/components/key-value-table/key-value-table.tsx
@@ -8,25 +8,29 @@ interface KeyValueTableProps extends HTMLAttributes<HTMLTableElement> {
 
 export const KeyValueTable = ({ rows, style }: KeyValueTableProps) => {
   return (
-    <table style={{ fontSize: 14, ...style }}>
-      <tbody>
-        {rows.map((row, i) => (
-          <tr key={i}>
-            <th style={{ textAlign: 'left', color: Colors.WHITE }}>
-              {row.key}:
-            </th>
-            <td
-              style={{
-                textAlign: 'right',
-                color: Colors.TEXT_COLOR_DEEMPHASISE
-              }}
-              data-testid={row.dataTestId}
-            >
-              {row.value}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <dl
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gridGap: 10,
+        fontSize: 14,
+        ...style
+      }}
+    >
+      {rows.map(row => (
+        <>
+          <dt style={{ textAlign: 'left', color: Colors.WHITE }}>{row.key}:</dt>
+          <dd
+            style={{
+              textAlign: 'right',
+              color: Colors.TEXT_COLOR_DEEMPHASISE
+            }}
+            data-testid={row.dataTestId}
+          >
+            {row.value}
+          </dd>
+        </>
+      ))}
+    </dl>
   )
 }
