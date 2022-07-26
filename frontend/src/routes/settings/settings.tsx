84b36c75789a8cd2e1ec8260cd3a38e0ae6ed329
Matthew Russell <mattrussell36@gmail.com>
2022-07-26 17:35:53 +0100

    feat: separate form from container, add hook

--- frontend/src/routes/settings/settings.tsx
+++ frontend/src/routes/settings/settings.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 
@@ -8,9 +9,46 @@ import { FormGroup } from '../../components/form-group'
 import { Select } from '../../components/forms'
 import { Input } from '../../components/forms/input'
 import { RadioGroup } from '../../components/radio-group'
+import { AppToaster } from '../../components/toaster'
+import { Intent } from '../../config/intent'
+import { LogLevels } from '../../config/log-levels'
 import { useGlobal } from '../../contexts/global/global-context'
+import { createLogger } from '../../lib/logging'
 import * as Service from '../../wailsjs/go/backend/Handler'
 import { config as ConfigModel } from '../../wailsjs/go/models'
+
+const logger = createLogger('Settings')
+
+const useUpdateConfig = () => {
+  const [status, setStatus] = useState<
+    'default' | 'pending' | 'success' | 'error'
+  >('default')
+  const submit = async (fields: FormFields) => {
+    try {
+      logger.debug('UpdateAppConfig')
+      setStatus('pending')
+      await Service.UpdateAppConfig(
+        new ConfigModel.Config({
+          vegaHome: fields.vegaHome,
+          logLevel: fields.logLevel
+          // TODO: Fix me. Backend is broken here
+          // telemetry: {
+          //   enabled: fields.telemetry === 'yes' ? true : false
+          // }
+        })
+      )
+      AppToaster.show({ message: 'Config updated', intent: Intent.SUCCESS })
+      setStatus('success')
+    } catch (err) {
+      const message = 'Failed to update config'
+      AppToaster.show({ message, intent: Intent.DANGER })
+      logger.error(err)
+      setStatus('error')
+    }
+  }
+
+  return { submit, status }
+}
 
 interface FormFields {
   vegaHome: string
@@ -23,6 +61,41 @@ export function Settings() {
     state: { config }
   } = useGlobal()
   const navigate = useNavigate()
+
+  const { submit, status } = useUpdateConfig()
+  const isPending = status === 'pending'
+
+  if (!config) {
+    return null
+  }
+
+  return (
+    <Dialog open={true}>
+      <div>
+        <SettingsForm
+          config={config}
+          onSubmit={submit}
+          onCancel={() => navigate(-1)}
+          isPending={isPending}
+        />
+      </div>
+    </Dialog>
+  )
+}
+
+interface SettingsFormProps {
+  onSubmit: (fields: FormFields) => void
+  onCancel: () => void
+  config: ConfigModel.Config
+  isPending: boolean
+}
+
+const SettingsForm = ({
+  onSubmit,
+  onCancel,
+  config,
+  isPending
+}: SettingsFormProps) => {
   const { control, register, handleSubmit } = useForm<FormFields>({
     defaultValues: {
       vegaHome: config?.vegaHome,
@@ -30,65 +103,48 @@ export function Settings() {
       telemetry: config?.telemetry.enabled ? 'yes' : 'no'
     }
   })
-
-  const onSubmit = async (fields: FormFields) => {
-    console.log(fields)
-    try {
-      const res = await Service.UpdateAppConfig(
-        new ConfigModel.Config({
-          vegaHome: fields.vegaHome,
-          logLevel: fields.logLevel
-        })
-      )
-      console.log(res)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
   return (
-    <Dialog open={true}>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormGroup
-            label='Wallet directory'
-            labelFor='vegaHome'
-            helperText='This specifies where the app writes and reads its data. Warning! Changing this will remove existing wallets.'
-          >
-            <Input {...register('vegaHome')} />
-          </FormGroup>
-          <FormGroup
-            label='Log level'
-            labelFor='logLevel'
-            helperText='Logs can be found at ~/.vega/logs'
-          >
-            <Select {...register('logLevel')}>
-              <option value='info'>Info</option>
-              <option value='debug'>Debug</option>
-              <option value='warn'>Warn</option>
-              <option value='error'>Error</option>
-            </Select>
-          </FormGroup>
-          <FormGroup
-            label='Report bugs and crashes'
-            labelFor='telemetry'
-            helperText='Selecting yes will help developers improve the software'
-          >
-            <RadioGroup
-              name='telemetry'
-              options={[
-                { label: 'Yes', value: 'yes' },
-                { label: 'No', value: 'no' }
-              ]}
-              control={control as any}
-            />
-          </FormGroup>
-          <ButtonGroup>
-            <Button type='submit'>Update</Button>
-            <Button onClick={() => navigate(-1)}>Cancel</Button>
-          </ButtonGroup>
-        </form>
-      </div>
-    </Dialog>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormGroup
+        label='Wallet directory'
+        labelFor='vegaHome'
+        helperText='This specifies where the app writes and reads its data. Warning! Changing this will remove existing wallets.'
+      >
+        <Input {...register('vegaHome')} />
+      </FormGroup>
+      <FormGroup
+        label='Log level'
+        labelFor='logLevel'
+        helperText='Logs can be found at ~/.vega/logs'
+      >
+        <Select {...register('logLevel')}>
+          {Object.entries(LogLevels).map(([key, value]) => (
+            <option value={value} key={key}>
+              {key}
+            </option>
+          ))}
+        </Select>
+      </FormGroup>
+      <FormGroup
+        label='Report bugs and crashes'
+        labelFor='telemetry'
+        helperText='Selecting yes will help developers improve the software'
+      >
+        <RadioGroup
+          name='telemetry'
+          options={[
+            { label: 'Yes', value: 'yes' },
+            { label: 'No', value: 'no' }
+          ]}
+          control={control as any}
+        />
+      </FormGroup>
+      <ButtonGroup>
+        <Button type='submit' disabled={isPending} loading={isPending}>
+          Update
+        </Button>
+        <Button onClick={onCancel}>Cancel</Button>
+      </ButtonGroup>
+    </form>
   )
 }
