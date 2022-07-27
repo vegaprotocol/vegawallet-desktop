80c4b4336da2558873d9098217d73f992140cb31
Matthew Russell <mattrussell36@gmail.com>
2022-07-27 20:06:38 +0100

    feat: split form component from page

--- frontend/src/routes/wallet/delete.tsx
+++ frontend/src/routes/wallet/delete.tsx
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react'
-import { logger } from '@sentry/utils'
 import { useCallback } from 'react'
 import { useForm } from 'react-hook-form'
 import { Navigate, useNavigate } from 'react-router-dom'
@@ -13,76 +12,12 @@ import { Intent } from '../../config/intent'
 import { useGlobal } from '../../contexts/global/global-context'
 import { useCurrentKeypair } from '../../hooks/use-current-keypair'
 import { FormStatus, useFormState } from '../../hooks/use-form-state'
+import { createLogger } from '../../lib/logging'
 import * as Service from '../../wailsjs/go/backend/Handler'
 import { backend as BackendModel } from '../../wailsjs/go/models'
 import { Paths } from '..'
 
-interface FormFields {
-  confirmText: string
-}
-
-export const Delete = () => {
-  const navigate = useNavigate()
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<FormFields>()
-  const { wallet } = useCurrentKeypair()
-  const { submit, status } = useDeleteWallet()
-
-  if (!wallet) {
-    return <Navigate to={Paths.Home} />
-  }
-
-  const confirmText = `Delete ${wallet.name}`
-  const isPending = status === FormStatus.Pending
-
-  return (
-    <div style={{ padding: 20 }}>
-      <p style={{ marginBottom: 15 }}>
-        Are you sure you want to delete this wallet?
-      </p>
-      <p style={{ marginBottom: 15 }}>
-        Doing so will permanently delete this wallet and you will only be able
-        to recover assets if you have a back up phrase.
-      </p>
-      <p style={{ marginBottom: 15 }}>
-        You may want to create a copy of your recovery phrase before you
-        continue.
-      </p>
-      <form onSubmit={handleSubmit(() => submit(wallet.name))}>
-        <FormGroup
-          label={`Type: "${confirmText}" to permanently delete this wallet`}
-          labelFor='confirmText'
-          helperText={errors.confirmText?.message}
-          intent={errors.confirmText?.message ? Intent.DANGER : Intent.NONE}
-        >
-          <Input
-            id='confirmText'
-            {...register('confirmText', {
-              required: 'Required',
-              validate: {
-                confirmText: value => {
-                  if (value === confirmText) {
-                    return true
-                  }
-                  return 'Invalid confirmation text'
-                }
-              }
-            })}
-          />
-        </FormGroup>
-        <ButtonGroup>
-          <Button type='submit' disabled={isPending} loading={isPending}>
-            Delete
-          </Button>
-          <Button onClick={() => navigate(-1)}>Cancel</Button>
-        </ButtonGroup>
-      </form>
-    </div>
-  )
-}
+const logger = createLogger('Delete')
 
 const useDeleteWallet = () => {
   const navigate = useNavigate()
@@ -118,4 +53,94 @@ const useDeleteWallet = () => {
   )
 
   return { status, submit }
+}
+
+interface FormFields {
+  confirmText: string
+}
+
+export const Delete = () => {
+  const navigate = useNavigate()
+  const { wallet } = useCurrentKeypair()
+  const { submit, status } = useDeleteWallet()
+
+  if (!wallet) {
+    return <Navigate to={Paths.Home} />
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <p style={{ marginBottom: 15 }}>
+        Are you sure you want to delete this wallet?
+      </p>
+      <p style={{ marginBottom: 15 }}>
+        Doing so will permanently delete this wallet and you will only be able
+        to recover assets if you have a back up phrase.
+      </p>
+      <p style={{ marginBottom: 15 }}>
+        You may want to create a copy of your recovery phrase before you
+        continue.
+      </p>
+      <DeleteForm
+        walletName={wallet.name}
+        status={status}
+        onSubmit={() => submit(wallet.name)}
+        onCancel={() => navigate(-1)}
+      />
+    </div>
+  )
+}
+
+interface DeleteFormProps {
+  walletName: string
+  status: FormStatus
+  onSubmit: () => void
+  onCancel: () => void
+}
+
+const DeleteForm = ({
+  onSubmit,
+  status,
+  onCancel,
+  walletName
+}: DeleteFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormFields>()
+  const confirmText = `Delete ${walletName}`
+  const isPending = status === FormStatus.Pending
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormGroup
+        label={`Type: "${confirmText}" to permanently delete this wallet`}
+        labelFor='confirmText'
+        helperText={errors.confirmText?.message}
+        intent={errors.confirmText?.message ? Intent.DANGER : Intent.NONE}
+      >
+        <Input
+          id='confirmText'
+          {...register('confirmText', {
+            required: 'Required',
+            validate: {
+              confirmText: value => {
+                if (value === confirmText) {
+                  return true
+                }
+                return 'Invalid confirmation text'
+              }
+            }
+          })}
+        />
+      </FormGroup>
+      <ButtonGroup>
+        <Button type='submit' disabled={isPending} loading={isPending}>
+          Delete
+        </Button>
+        <Button onClick={onCancel}>Cancel</Button>
+      </ButtonGroup>
+    </form>
+  )
 }
