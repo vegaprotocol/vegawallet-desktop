45d4d5719a7824eea9e6cbb04514ef0ab3dde505
Valentin Trinque <val.trinque@gmail.com>
2022-08-11 17:04:58 +0200

    Revert "fix: Ensure a saved configuration set the consent asked property to true"
    This reverts commit 5338551b99e04a3fca9d7577fee736ebea6b566f.

--- backend/config.go
+++ backend/config.go
@@ -57,8 +57,6 @@ func (h *Handler) UpdateAppConfig(cfg *config.Config) error {
 	h.log.Debug("Entering UpdateAppConfig")
 	defer h.log.Debug("Leaving UpdateAppConfig")
 
-	cfg.Telemetry.ConsentAsked = true
-
 	if !isSupportedLogLevel(cfg.LogLevel) {
 		return fmt.Errorf("unsupported logger level %s", cfg.LogLevel)
 	}
