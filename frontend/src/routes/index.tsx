3c39dfbd28b3690e93bea7f5271a2668cb901519
Matthew Russell <mattrussell36@gmail.com>
2022-08-18 12:53:25 +0200

    chore: remove unnecessary fragment

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -30,39 +30,37 @@ export enum Paths {
 
 export const AppRouter = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Outlet />}>
-          <Route index={true} element={<Home />} />
-          <Route path='onboard' element={<Onboard />}>
-            <Route index={true} element={<OnboardHome />} />
-            <Route path='settings' element={<OnboardSettings />} />
-            <Route path='wallet-create' element={<OnboardWalletCreate />} />
-            <Route path='wallet-import' element={<OnboardWalletImport />} />
-            <Route path='network' element={<OnboardNetwork />} />
-          </Route>
-          <Route path='wallet-create' element={<WalletCreate />} />
-          <Route path='wallet-import' element={<WalletImport />} />
-          <Route path='network-import' element={<NetworkImport />} />
-          <Route path='wallet/:wallet' element={<Wallet />}>
-            <Route
-              index={true}
-              element={
-                <Center>
-                  <p>Select a key</p>
-                </Center>
-              }
-            />
-            <Route path='delete' element={<Delete />} />
-            <Route path='keypair/:pubkey' element={<WalletKeyPair />}>
-              <Route index={true} element={<KeyPairHome />} />
-              <Route path='sign' element={<Sign />} />
-              <Route path='taint' element={<Taint />} />
-              <Route path='metadata' element={<Metadata />} />
-            </Route>
+    <Routes>
+      <Route path='/' element={<Outlet />}>
+        <Route index={true} element={<Home />} />
+        <Route path='onboard' element={<Onboard />}>
+          <Route index={true} element={<OnboardHome />} />
+          <Route path='settings' element={<OnboardSettings />} />
+          <Route path='wallet-create' element={<OnboardWalletCreate />} />
+          <Route path='wallet-import' element={<OnboardWalletImport />} />
+          <Route path='network' element={<OnboardNetwork />} />
+        </Route>
+        <Route path='wallet-create' element={<WalletCreate />} />
+        <Route path='wallet-import' element={<WalletImport />} />
+        <Route path='network-import' element={<NetworkImport />} />
+        <Route path='wallet/:wallet' element={<Wallet />}>
+          <Route
+            index={true}
+            element={
+              <Center>
+                <p>Select a key</p>
+              </Center>
+            }
+          />
+          <Route path='delete' element={<Delete />} />
+          <Route path='keypair/:pubkey' element={<WalletKeyPair />}>
+            <Route index={true} element={<KeyPairHome />} />
+            <Route path='sign' element={<Sign />} />
+            <Route path='taint' element={<Taint />} />
+            <Route path='metadata' element={<Metadata />} />
           </Route>
         </Route>
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   )
 }
