501be6f74aad111439ec24fd81594a2ab7e881b9
Matthew Russell <mattrussell36@gmail.com>
2022-09-14 11:12:18 -0700

    chore: remove apollo client setup and fetching of asset data

--- frontend/src/routes/wallet/keypair/home.tsx
+++ frontend/src/routes/wallet/keypair/home.tsx
@@ -1,9 +1,7 @@
 import { BreakText } from '../../../components/break-text'
 import { Header } from '../../../components/header'
 import { KeyValueTable } from '../../../components/key-value-table'
-import { useAccounts } from '../../../hooks/use-accounts'
 import { useCurrentKeypair } from '../../../hooks/use-current-keypair'
-import { addDecimal } from '../../../lib/number'
 
 export function KeyPairHome() {
   const { keypair } = useCurrentKeypair()
@@ -25,56 +23,6 @@ export function KeyPairHome() {
           }
         ]}
       />
-      <Header>Assets</Header>
-      <AccountsTable publicKey={keypair.publicKey} />
     </div>
-  )
-}
-
-interface AccountsTableProps {
-  publicKey: string
-}
-
-function AccountsTable({ publicKey }: AccountsTableProps) {
-  const { accounts, loading, error } = useAccounts(publicKey)
-
-  if (error) {
-    return <p>Could not retrieve account information</p>
-  }
-
-  if (loading) {
-    return <p>Loading accounts</p>
-  }
-
-  const entries = Object.entries(accounts)
-
-  if (!entries.length) {
-    return <p>No accounts</p>
-  }
-
-  return (
-    <>
-      {entries.map(([id, accounts]) => {
-        return (
-          <div key={id} style={{ marginBottom: 20 }}>
-            <Header
-              data-testid='asset-name'
-              style={{ fontSize: 14, margin: 0 }}
-            >
-              {accounts[0].asset.name}
-            </Header>
-            <KeyValueTable
-              data-testid='assets-table'
-              rows={accounts.map(a => {
-                return {
-                  key: a.type,
-                  value: addDecimal(a.balance, a.asset.decimals)
-                }
-              })}
-            />
-          </div>
-        )
-      })}
-    </>
   )
 }
