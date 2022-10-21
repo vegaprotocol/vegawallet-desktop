82c6581f30b523d6753378b36c095930befe8d7b
AndyWhiteVega <andy@vegaprotocol.io>
2022-10-21 15:28:04 +0100

    test: fix

--- frontend/automation/cypress/e2e/settings.cy.js
+++ frontend/automation/cypress/e2e/settings.cy.js
@@ -28,7 +28,7 @@ describe('settings', () => {
     cy.getByTestId(settingsForm).should('be.visible')
 
     // assert and change log level
-    cy.getByTestId('log-level').should('have.value', 'info').select('debug')
+    cy.getByTestId('log-level').last().should('have.value', 'info').select('debug')
 
     // change telemetry
     const radioGroupSelector = '[role="radiogroup"]'
@@ -42,7 +42,7 @@ describe('settings', () => {
     cy.getByTestId(settingsForm).should('not.exist')
 
     cy.getByTestId(homeSettingsBtn).click()
-    cy.getByTestId('log-level').should('have.value', 'debug')
+    cy.getByTestId('log-level').last().should('have.value', 'debug')
     cy.get(radioGroupSelector).find('input[value="yes"]').should('be.checked')
     cy.getByTestId(cancelSettingsBtn).click()
   })
