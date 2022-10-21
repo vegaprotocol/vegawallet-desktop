488369bab91fd702b1a1f437793396f4dd96c3e7
Botond <botond@vegaprotocol.io>
2022-10-21 15:06:58 +0100

    fix: cleanup settings test

--- frontend/automation/cypress/e2e/settings.cy.js
+++ frontend/automation/cypress/e2e/settings.cy.js
@@ -4,28 +4,19 @@ describe('settings', () => {
   const homeSettingsBtn = 'home-settings'
   const settingsForm = 'settings-form'
   const cancelSettingsBtn = 'cancel-settings'
-  let passphrase
-  let walletName
 
   before(() => {
     cy.clean()
     cy.backend()
       .then(handler => {
         cy.setVegaHome(handler)
-        // cy.restoreNetwork(handler)
-        // cy.restoreWallet(handler)
       })
       .then(() => {
         cy.waitForHome()
       })
   })
 
-  beforeEach(() => {
-    passphrase = Cypress.env('testWalletPassphrase')
-    walletName = Cypress.env('testWalletName')
-  })
-
-  it.skip('dialog opens and can be closed', () => {
+  it('dialog opens and can be closed', () => {
     cy.getByTestId(homeSettingsBtn).click()
     cy.getByTestId(settingsForm).should('be.visible')
     cy.getByTestId(cancelSettingsBtn).click()
@@ -42,17 +33,17 @@ describe('settings', () => {
     // change telemetry
     const radioGroupSelector = '[role="radiogroup"]'
     cy.get(radioGroupSelector).find('input[value="no"]').should('be.checked')
-    // cy.get(radioGroupSelector).find('button[value="yes"]').click()
+    cy.get(radioGroupSelector).find('button[value="yes"]').click()
 
-    // // submit
-    // cy.getByTestId('update-settings').click()
-    //
-    // // page should reload and settings form should now not show
-    // cy.getByTestId(settingsForm).should('not.exist')
-    //
-    // cy.getByTestId(homeSettingsBtn).click()
-    // cy.getByTestId('log-level').should('have.value', 'debug')
-    // cy.get(radioGroupSelector).find('input[value="yes"]').should('be.checked')
-    // cy.getByTestId(cancelSettingsBtn).click()
+    // submit
+    cy.getByTestId('update-settings').click()
+
+    // page should reload and settings form should now not show
+    cy.getByTestId(settingsForm).should('not.exist')
+
+    cy.getByTestId(homeSettingsBtn).click()
+    cy.getByTestId('log-level').should('have.value', 'debug')
+    cy.get(radioGroupSelector).find('input[value="yes"]').should('be.checked')
+    cy.getByTestId(cancelSettingsBtn).click()
   })
 })
