2ab6d0d4bd1337a8dd6f60dd0b40516dee213ce3
Botond <botond@vegaprotocol.io>
2022-11-02 11:15:20 +0000

    fix: format

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,15 +5,15 @@ import { useEffect } from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 
 import { AppFrame, AppLoader } from './app-loader'
+import { Button } from './components/button'
 import { Chrome } from './components/chrome'
 import { InteractionManager } from './components/interaction-manager'
 import { NetworkCompatibilityDialog } from './components/network-compatibility-dialog'
 import { PassphraseModal } from './components/passphrase-modal'
 import { Settings } from './components/settings'
+import { SplashError } from './components/splash-error'
 import { TelemetryDialog } from './components/telemetry-dialog'
 import { GlobalProvider } from './contexts/global/global-provider'
-import { SplashError } from './components/splash-error'
-import { Button } from './components/button'
 import { createLogger, initLogger } from './lib/logging'
 import { AppRouter } from './routes'
 import { Service } from './service'
@@ -55,13 +55,9 @@ function App() {
     <ErrorBoundary
       fallback={({ error }) => (
         <SplashError
-          title="Somthing went wrong"
+          title='Somthing went wrong'
           message={error.message}
-          actions={[
-            <Button onClick={WindowReload}>
-              Reload
-            </Button>
-          ]}
+          actions={[<Button onClick={WindowReload}>Reload</Button>]}
         />
       )}
     >
