38b9f51d6a7e3ef21a24bcf08a658e81213d8bd9
AndyWhiteVega <andy@vegaprotocol.io>
2022-11-02 14:45:04 +0000

    test: disable crash for uncaught exception

--- frontend/automation/cypress/support/e2e.js
+++ frontend/automation/cypress/support/e2e.js
@@ -1,3 +1,8 @@
 import '@cypress/code-coverage/support'
 import './commands'
 import './setup'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false
+})
