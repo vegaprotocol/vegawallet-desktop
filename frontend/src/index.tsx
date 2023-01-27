e051b00e980d207385abbce21cac17b9f312e282
Botond <botond@vegaprotocol.io>
2023-01-27 14:49:08 +0000

    fix: remove react strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,25 +1,14 @@
 import 'core-js/stable'
-
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './app'
-import { ENV } from './config/environment'
 
 const element = document.getElementById('app')
 
 if (element) {
   const root = createRoot(element)
 
-  if (ENV === 'development') {
-    root.render(<App />)
-  } else {
-    root.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    )
-  }
+  root.render(<App />)
 } else {
   throw Error('Could not find root element with id "app".')
 }
