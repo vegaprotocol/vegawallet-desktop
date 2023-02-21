62914d3c0be82e5120cdf6261a55d36b0416da88
Karel Moravec <moravec.wdd@gmail.com>
2023-02-21 16:30:44 +0100

    feat: add hide window to menu on Mac

--- backend/app_menu.go
+++ backend/app_menu.go
@@ -1,9 +1,8 @@
-//go:build !darwin
-
 package backend
 
 import (
 	"fmt"
+	"runtime"
 
 	"code.vegaprotocol.io/vegawallet-desktop/app"
 	"github.com/wailsapp/wails/v2/pkg/menu"
@@ -17,9 +16,21 @@ func (h *Handler) AppMenu() *menu.Menu {
 	mainMenu := appMenu.AddSubmenu("Main")
 	mainMenu.AddText("About Vegawallet", nil, h.openAboutDialog)
 	mainMenu.AddSeparator()
+
+	if runtime.GOOS == "darwin" {
+		mainMenu.AddText("Hide Vegawallet", keys.CmdOrCtrl("h"), func(_ *menu.CallbackData) {
+			wailsRuntime.Hide(h.ctx)
+		})
+		mainMenu.AddSeparator()
+	}
+
 	mainMenu.AddText("Quit", keys.CmdOrCtrl("q"), func(_ *menu.CallbackData) {
 		wailsRuntime.Quit(h.ctx)
 	})
+
+	if runtime.GOOS == "darwin" {
+		appMenu.Append(menu.EditMenu()) // on macos platform, we should append EditMenu to enable Cmd+C,Cmd+V,Cmd+Z... shortcut
+	}
 
 	h.commonAppMenu(appMenu)
 
