bf235b8b73b699520910e2caebe45cf7d69ad91b
Rado <szpiechrados@gmail.com>
2023-03-28 18:50:30 +0200

    chore: increase setup timeout

--- frontend/automation/playwright.config.ts
+++ frontend/automation/playwright.config.ts
@@ -91,6 +91,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev:test',
-    port: 34115
+    port: 34115,
+    timeout: 5 * 60 * 1000
   }
 })
