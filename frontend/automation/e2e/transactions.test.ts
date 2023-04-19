47a0bc514d4d6d8c5c8da46108f63d632697bd0d
Rado <szpiechrados@gmail.com>
2023-04-19 17:00:47 +0200

    chore: prettier

--- frontend/automation/e2e/transactions.test.ts
+++ frontend/automation/e2e/transactions.test.ts
@@ -1,15 +1,17 @@
-import {expect,test} from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 import initApp from '../support/init-app'
 
 test.describe('transactions view', () => {
-    test.beforeEach(async ({ page }) => {
-        await initApp(page)
-        await page.goto('/')
-    })
+  test.beforeEach(async ({ page }) => {
+    await initApp(page)
+    await page.goto('/')
+  })
 
-test('can navigate to transactions page', async ({page}) => {
+  test('can navigate to transactions page', async ({ page }) => {
     await page.locator('a[href="#/transactions"]').click()
-    await expect(page.getByTestId('Transactions-header')).toHaveText('Transactions')
+    await expect(page.getByTestId('Transactions-header')).toHaveText(
+      'Transactions'
+    )
   })
 })
